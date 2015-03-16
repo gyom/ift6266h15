226fd3fe055a6c1b716e1b4b39fc449e6b3d1df4
Guillaume Alain <gyomalin@gmail.com>
2015-03-16 16:22:39 -0400

    Got the thing running, but it seems that the parameters are not being saved in a way that allows me to read back the convolution filters from the multiple layers. I suspect that it's "ConvolutionalSequence" that eats everything and gives weird names to the filters.

---
+++ code/blocks/dataset.py
@@ -0,0 +1,72 @@
+
+# taken from https://github.com/bartvm/dogs-vs-cats/
+
+import os
+
+import h5py
+
+from fuel import config
+from fuel.datasets import InMemoryDataset
+
+
+@InMemoryDataset.lazy_properties('features', 'targets')
+class DogsVsCats(InMemoryDataset):
+    provides_sources = ('features', 'targets')
+
+    def __init__(self, which_set):
+
+        # flip this switch to debug your algorithms faster
+        # by using less data
+        want_truncated_version = True
+
+        if want_truncated_version:
+            if which_set == 'train':
+                self.start = 0
+                self.stop = self.start + 200
+            elif which_set == 'valid':
+                self.start = 20000
+                self.stop = self.start + 100
+            elif which_set == 'test':
+                self.start = 22500
+                self.stop = self.start + 100
+            else:
+                raise ValueError
+        else:
+            if which_set == 'train':
+                self.start = 0
+                self.stop = 20000
+            elif which_set == 'valid':
+                self.start = 20000
+                self.stop = 22500
+            elif which_set == 'test':
+                self.start = 22500
+                self.stop = 25000
+            else:
+                raise ValueError
+        self.f = h5py.File(os.path.join(config.data_path, 'dogs_vs_cats',
+                                        'dogs_vs_cats.hdf5'))
+
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        del state['f']
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+        self.f = h5py.File(os.path.join(config.data_path, 'dogs_vs_cats',
+                                        'dogs_vs_cats.hdf5'))
+
+    @property
+    def num_examples(self):
+        return self.stop - self.start
+
+    def get_data(self, state=None, request=None):
+        if state is not None:
+            raise ValueError
+        images, targets = [], []
+        request = sorted([i + self.start for i in request])
+        for image, shape, target in zip(self.f['images'][request],
+                                        self.f['shapes'][request],
+                                        self.f['labels'][request]):
+            images.append(image.reshape(shape))
+            targets.append([target])
+        return self.filter_sources((images, targets))
